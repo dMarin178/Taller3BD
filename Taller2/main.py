7b5b49f2e3e5ba5b5a726d1a899c19abc3413483
Dino Marin <dinomarindiaz178@gmail.com>
2020-07-06 01:37:37 -0400

    Login y sign up terminado

--- Taller2/main.py
+++ Taller2/main.py
@@ -1,7 +1,12 @@
 from __future__ import unicode_literals
 import time
 from conexion import conexionEjemplo
-from conexion import conectar()
+from conexion import conectar
+from inputs import inputDatosRegistro
+from inputs import inputNewNick
+from inputs import inputEmail
+
+conexionEjemplo()
 
 def menu():
     print("Bienvenido a El Bruto \n")
@@ -9,37 +14,34 @@ def menu():
     print("2) Registrarse")
     print("0) Salir")
 
+#devuelve el perfil(admin o jugador) si no lo encuentra devulve None
 def IniciarSesion(nick,password) :
     inicio= False
     perfil = None
-        cur = conectar()
-        cur.execute("select nick,contraseña from administrador ")
-        # recorremos el cursor en la tabla de administrador
-        for administrador in cur :
-            if(administrador[0]==nick):
-                if(administrador[1]==password):
+    conn = conectar()
+    cur = conn.cursor()
+    cur.execute("select nick,contraseña from administrador ")
+    # recorremos el cursor en la tabla de administrador
+    for administrador in cur :
+        if(administrador[0]==nick):
+            if(administrador[1]==password):
+                #print("\n Bienvenido "+nick+"\n")
+                inicio=True
+                perfil = "administrador"
+    #si no se encuentra ningun administrador con el nick correspondiente,
+    #recorremos a los jugadores       
+    if(inicio==False):
+        cur.execute("select nick,contraseña from jugador ")
+        for jugador in cur:
+            if(jugador[0]==nick):
+                if(jugador[1]==password):
                     #print("\n Bienvenido "+nick+"\n")
                     inicio=True
-                    perfil = "administrador"
-        #si no se encuentra ningun administrador con el nick correspondiente,
-        #recorremos a los jugadores       
-        if(inicio==False):
-            cur.execute("select nick,contraseña from jugador ")
-            for jugador in cur:
-                if(jugador[0]==nick):
-                    if(jugador[1]==password):
-                        #print("\n Bienvenido "+nick+"\n")
-                        inicio=True
-                        perfil = "jugador"
-        # Cerrar el cursor
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            return perfil
+                    perfil = "jugador"
+    conn.close()                    
+    return perfil
 
+#Registra al usuario en la base de datos, recibe una lista con los datos a ingresar
 def RegistrarUsuario(datosDeRegistro):
     data = datosDeRegistro
     conn = conectar()
@@ -57,6 +59,19 @@ def menuAdmin(nick):
 def menuJugador(nick):
     print("Este es el menu del jugador \n ")
 
+def getAvatar(nick):
+    conn = conectar()
+    cur = conn.cursor()
+    cur.execute("select * from avatar")
+    found = False
+    for avatar in cur :
+        if(avatar[0]==nick):
+            found = True
+            atributes = [avatar[0],avatar[1],avatar[2],avatar[3],avatar[4]] 
+            return atributes     
+    if(found==False):
+        print("No se encontro al avatar")
+
 def principal() :
     opcion = -1
     while opcion != "0" :
@@ -68,52 +83,19 @@ def principal() :
             nick = input()
             print("Ingrese su contraseña : ")
             password = input()
-            sesion = IniciarSesion(nick,password)
-            if sesion == None :
+            perfil = IniciarSesion(nick,password)
+            if perfil == None :
                 print("Nick o contraseña incorrectos \n")
-            elif sesion == "administrador" :
+            elif perfil == "administrador" :
                 menuAdmin(nick)
             else : 
-                menuJugador(nick)    
+                menuJugador(nick)   
         elif opcion == "2" :
-            print("Ingrese su nick con el que iniciara sesion : ")
-            nick = input()
-            print("Ingrese sus nombres : ")
-            nombres = input()
-            print("Ingrese su apellido paterno :")
-            apellidoP = input()
-            print("Ingrese su apellido materno : ")
-            apellidoM = input()
-            correo = emailVerification()
-            print("Ingrese su país : ")
-            pais = input()
-            print("Ingrese su constraseña : ")
-            password = input()
-            RegistrarUsuario(nick,nombres,apellidoP,apellidoM,correo,password,pais)
+            data = inputDatosRegistro()
+            RegistrarUsuario(data)
         elif opcion == "0" :
             print("Adios , nos vemos pronto para mas lucha ")
         else :
             print("Ingrese una opcion valida")
- 
-def getAvatar(nick):
-    try:
-        conn = conectar()
-        cur = conn.cursor()
-        cur.execute("select * from avatar")
-        found = False
-        for avatar in cur :
-            if(avatar[0]==nick):
-                found = True
-                atributes = [avatar[0],avatar[1],avatar[2],avatar[3],avatar[4]] 
-                return atributes     
-        if(found==False):
-            print("No se encontro al usuario")
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
-conexionEjemplo()
-
+principal()
