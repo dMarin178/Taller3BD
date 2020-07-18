aa59c3cb1070fa595a82129fbd5b7df548a07b74
Dino Marin <dinomarindiaz178@gmail.com>
2020-07-18 18:44:58 -0400

    Menu de jugador terminado

--- Taller2/sesion.py
+++ Taller2/sesion.py
@@ -2,23 +2,58 @@ import tkinter as tk
 from tkinter.messagebox import showinfo
 from functools import partial
 from main import getAvatar
+import tkinter.font as tkFont
 
 def menuJugador(nick):
-    window = tk.Tk()
-    window.title("Perfil de "+nick)
-    window.geometry("400x400")
+    sesionJugador = tk.Tk()
+    sesionJugador.title("Perfil de "+nick)
+    sesionJugador.geometry("400x400")
     #titulo del menu
-    window.configure(background = 'black')
+    sesionJugador.configure(background = 'black')
 
     avatar= getAvatar(nick)
 
-    titulo = tk.Label(window, text = nick ,bg='black',fg='white')
-    titulo.pack(pady=10)
+    titulo = tk.Label(sesionJugador, text = nick ,bg='black',fg='white', font= tkFont.Font(size=20))
+    titulo.pack(pady=20)
 
-    nickLabel = tk.Label(text="Puntos de ataque "+str(avatar[0]),bg='black',fg='white')
-    nickLabel.pack()
+    ptsAtck = tk.Label(sesionJugador ,text="Puntos de ataque : "+str(avatar[1]),bg='black',fg='white')
+    ptsAtck.pack(pady=5)
 
-    passLabel = tk.Label(text="Puntos de defensa"+str(avatar[1]),bg='black',fg='white')
-    passLabel.pack()
+    ptsDef = tk.Label(sesionJugador,text="Puntos de vida : "+str(avatar[2]),bg='black',fg='white')
+    ptsDef.pack(pady=5)
 
-    window.mainloop()
+    ptosVel = tk.Label(sesionJugador,text="Puntos de velocidad : "+str(avatar[3]),bg='black',fg='white')
+    ptosVel.pack(pady=5)
+
+    lvl = tk.Label(sesionJugador,text="nivel : "+ str(getNivel(avatar[3])),bg='black',fg='white')
+    lvl.pack(pady=5)
+
+    ptosExp=tk.Label(sesionJugador,text="experiencia : "+str(avatar[3])+"/"+str(nextLvl(getNivel(avatar[3]))) ,bg='black',fg='white')
+    ptosExp.pack(pady=5)
+
+    reportarJugador = tk.Button(sesionJugador ,text="Reportar jugador",command=reportPlayer())
+    reportarJugador.pack(pady=10)
+
+    luchar= tk.Button(sesionJugador,text="Luchar")
+    luchar.pack(pady=10)
+
+    cerrarSesion= tk.Button(sesionJugador,text="Cerrar sesion")
+    cerrarSesion.pack(pady=10)
+
+    sesionJugador.mainloop()
+
+def reportPlayer():
+
+
+#devuelve el nivel que se encuentra el jugador
+def getNivel(ptosExperiencia):
+    if(ptosExperiencia < 150):
+        return 1
+    else:
+        nivel = abs((ptosExperiencia-100)//50)
+        return nivel
+
+#devuelve los puntos de experiencia necesarios para subir de nivel
+def nextLvl(nivel):
+    ptos=nivel*50+100
+    return ptos
